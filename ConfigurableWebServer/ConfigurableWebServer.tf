b1fda95ecb22f92d61c98496c083ab309ff5f241
Adrian Espinola <aespinolait@gmail.com>
2022-04-02 13:41:08 -0400

    Getting outputs and variables

--- ConfigurableWebServer/ConfigurableWebServer.tf
+++ ConfigurableWebServer/ConfigurableWebServer.tf
@@ -1,7 +1,21 @@
+#variables
+variable "server_port" {
+  description = "The port the server will user for HTTP requests"
+  type = number
+  default = 8080
+}
+
+output "server_data" {
+  description = "Public DNS and IP of instance"
+  value = [aws_instance.Server.public_dns,aws_instance.Server.public_ip]
+}
+
+# provider
 provider "aws" {
   region = "us-east-1"
 }
 
+#resources
 resource "aws_instance" "Server" {
   ami = "ami-0e472ba40eb589f49"
   instance_type = "t2.micro"
@@ -10,7 +24,7 @@ resource "aws_instance" "Server" {
   user_data = <<-EOF
     #!/bin/bash
     echo "Hello, World" > index.html
-    nohup busybox httpd -f -p 8080 &
+    nohup busybox httpd -f -p ${var.server_port} &
     EOF
 
   tags = {
@@ -21,8 +35,8 @@ resource "aws_instance" "Server" {
 resource "aws_security_group" "http" {
     name = "terraform-example-instance"
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
